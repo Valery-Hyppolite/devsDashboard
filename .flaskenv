8882dfdce763366ed808939f11903be850ef197f
Valery <valeryhypp@gmail.com>
2023-01-30 17:50:40 -0500

    UPDATE ENV FILE NAME

--- .flaskenv
+++
@@ -1,5 +0,0 @@
-FLASK_APP='app'
-#FLASK_DEBUG=1
-
-API_URL='http://yourdev.tk/api'
-#API_URL='http://127.0.0.1:8000/api'
--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from utils import getData
 
 load_dotenv()
-env_path = Path(".")/'.flaskenv'
+env_path = Path(".")/'.env'
 load_dotenv(dotenv_path=env_path)
 
 
